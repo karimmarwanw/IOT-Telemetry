376e1645e13ecf5c90801c2ad9c869210729d626
karimmarwanw <kokoomar900@gmail.com>
2025-10-27 01:36:08 +0300

    zabat el header format w zawedt protocol version

--- client.py
+++ client.py
@@ -6,6 +6,7 @@ import time
 INIT = 0
 DATA = 1
 HEARTBEAT = 2
+protocol_version = 1
 device_id = 1
 sequence_number = 0
 
@@ -13,17 +14,18 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 host = '127.0.0.1'
 port = 65432
 
-header_format = '!H I I H'
+header_format = '!B B H I B' # protocol_version, device_id, sequence_number, timestamp, msg_type
 
 def build_header(msg_type):
     global sequence_number
     sequence_number += 1
     timestamp = int(time.time())
-    return struct.pack(header_format, device_id, sequence_number, timestamp, msg_type)
+    return struct.pack(header_format, protocol_version, device_id, sequence_number, timestamp, msg_type)
 
 #INIT message
+message = "welcome to our IOT temperature sensor"
+init_payload = message.encode()
 init_header = build_header(INIT)
-init_payload = b"welcome to our IOT temperature sensor"
 s.sendto(init_header + init_payload, (host, port))
 print("INIT sent")
 
@@ -35,17 +37,21 @@ HEARTBEAT_INTERVAL = 1
 while True:
     now = time.time()
 
+    # Data message
     if now - last_data_time >= DATA_INTERVAL:
         temp = random.randint(34, 40)
+        data_payload = str(temp).encode()
         data_header = build_header(DATA)
-        payload = str(temp).encode()
-        s.sendto(data_header + payload, (host, port))
+        s.sendto(data_header + data_payload, (host, port))
         print(f"sent DATA: temp={temp}")
         last_data_time = now
 
+    # Heartbeat message
     if now - last_heartbeat_time >= HEARTBEAT_INTERVAL:
+        message = "the sensor is still alive"
+        heartbeat_payload = message.encode()
         heartbeat_header = build_header(HEARTBEAT)
-        s.sendto(heartbeat_header, (host, port))
+        s.sendto(heartbeat_header + heartbeat_payload, (host, port))
         print("sent HEARTBEAT")
         last_heartbeat_time = now
 
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 import struct
 import time
 
-header_format = '!H I I H'
+header_format = '!B B H I B'
 header_size = struct.calcsize(header_format)
 
 message_types = {
@@ -21,15 +21,16 @@ print(f"Server is listening on {host}:{port}")
 while True:
     data, address = s.recvfrom(1024)
 
-    header = data[:header_size] # make that first 12 bytes are the header bytes
+    header = data[:header_size] # make that first 9 bytes are the header bytes
     payload = data[header_size:] # and rest are payload
 
-    device_id, seq, timestamp, msg_type = struct.unpack(header_format, header)
+    protocol_version, device_id, seq, timestamp, msg_type= struct.unpack(header_format, header)
     msg_name = message_types.get(msg_type, "undefined") # if message type in message_types ok else undefined
 
     ts_str = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(timestamp))
 
-    print(f"packet from {address}")
+    print(f"packet from: {address}")
+    print(f"protocol version: {protocol_version}")
     print(f"device ID: {device_id}")
     print(f"sequence number: {seq}")
     print(f"timestamp: {ts_str}")
@@ -38,7 +39,10 @@ while True:
     if msg_name == "DATA":
         print(f"temperature: {payload.decode()}")
     elif msg_name == "HEARTBEAT":
-        print("this is a heartbeat payload")
+        print(f"payload: {payload.decode()}")
     elif msg_name == "INIT":
         print(f"payload: {payload.decode()}")
+    else :
+        print("unknown message type")
+
     print("-------------------------------------")
